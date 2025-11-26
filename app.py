afc6ba5620f7bde78ba858a083675f5787bb02fc
TweenaGirimalla <tweenagirimalla133@gmail.com>
2025-11-26 20:44:57 +0530

    files are commited

---
+++ app.py
@@ -0,0 +1,11 @@
+
+from flask import Flask
+app = Flask(_name_)
+
+@app.route("/")
+def home():
+    return "Welcome to Jenkins + Docker Integration!"
+
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=8080)
+
